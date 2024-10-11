29cf145dde3ad152f9830456468d4861fabc365a
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-10-11 22:32:15 +0900

    refactor: Fix todo

--- flutter_secure_storage_x/lib/options/android_options.dart
+++ flutter_secure_storage_x/lib/options/android_options.dart
@@ -71,12 +71,8 @@ class AndroidOptions extends Options {
   Map<String, String> toMap() => <String, String>{
         'encryptedSharedPreferences': '$_encryptedSharedPreferences',
         'resetOnError': '$_resetOnError',
-        // TODO: Update min SDK from 2.12 to 2.15 in new major version to fix this deprecation warning
-        // ignore: deprecated_member_use
-        'keyCipherAlgorithm': describeEnum(_keyCipherAlgorithm),
-        // TODO: Update min SDK from 2.12 to 2.15 in new major version to fix this deprecation warning
-        // ignore: deprecated_member_use
-        'storageCipherAlgorithm': describeEnum(_storageCipherAlgorithm),
+        'keyCipherAlgorithm': _keyCipherAlgorithm.name,
+        'storageCipherAlgorithm': _storageCipherAlgorithm.name,
         'sharedPreferencesName': sharedPreferencesName ?? '',
         'preferencesKeyPrefix': preferencesKeyPrefix ?? '',
       };
