71e4d46e954de9922ade0254c3c8d1e8d8b93e3b
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-10-12 11:43:48 +0900

    feat: Convert to kotlin (plugin)

---
+++ flutter_secure_storage_x/android/src/main/java/com/it_nomads/fluttersecurestorage/FlutterSecureStorage.kt
@@ -0,0 +1,259 @@
+package com.it_nomads.fluttersecurestorage
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.util.Base64
+import android.util.Log
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.it_nomads.fluttersecurestorage.ciphers.StorageCipher
+import com.it_nomads.fluttersecurestorage.ciphers.StorageCipherFactory
+import java.lang.Exception
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+
+class FlutterSecureStorage(private val applicationContext: Context) {
+  private var options = mutableMapOf<String, String>()
+
+  private var preferences: SharedPreferences? = null
+  private var storageCipher: StorageCipher? = null
+  private var storageCipherFactory: StorageCipherFactory? = null
+
+  private var elementPreferencesKeyPrefix: String =
+    "VGhpcyBpcyB0aGUgcHJlZml4IGZvciBhIHNlY3VyZSBzdG9yYWdlCg"
+  private var sharedPreferencesName: String = "FlutterSecureStorage"
+  private var failedToUseEncryptedSharedPreferences = false
+
+  fun getResetOnError(): Boolean {
+    return options["resetOnError"] == "true"
+  }
+
+  private fun getUseEncryptedSharedPreferences(): Boolean {
+    if (failedToUseEncryptedSharedPreferences) {
+      return false
+    }
+    return options["encryptedSharedPreferences"] == "true"
+  }
+
+  fun containsKey(key: String): Boolean {
+    ensureInitialized()
+    return preferences!!.contains(key)
+  }
+
+  fun addPrefixToKey(key: String): String {
+    return elementPreferencesKeyPrefix + "_" + key
+  }
+
+  fun read(key: String): String? {
+    ensureInitialized()
+
+    val rawValue = preferences!!.getString(key, null)
+    if (rawValue == null) {
+      return null
+    }
+
+    if (getUseEncryptedSharedPreferences()) {
+      return rawValue
+    }
+    return decodeRawValue(rawValue)
+  }
+
+  fun readAll(): Map<String, String> {
+    ensureInitialized()
+
+    val raw = preferences!!.all as Map<String, String>
+
+    val all = mutableMapOf<String, String>()
+    for (entry in raw.entries) {
+      val keyWithPrefix = entry.key
+      if (keyWithPrefix.contains(elementPreferencesKeyPrefix)) {
+        val key = entry.key.replaceFirst((elementPreferencesKeyPrefix + '_').toRegex(), "")
+        if (getUseEncryptedSharedPreferences()) {
+          all.put(key, entry.value)
+        } else {
+          val rawValue = entry.value
+          val value = decodeRawValue(rawValue)
+
+          all.put(key, value)
+        }
+      }
+    }
+    return all
+  }
+
+  fun write(key: String, value: String) {
+    ensureInitialized()
+
+    val editor = preferences!!.edit()
+
+    if (getUseEncryptedSharedPreferences()) {
+      editor.putString(key, value)
+    } else {
+      val result = storageCipher!!.encrypt(value.toByteArray(charset))
+      editor.putString(key, Base64.encodeToString(result, 0))
+    }
+    editor.apply()
+  }
+
+  fun delete(key: String) {
+    ensureInitialized()
+
+    val editor = preferences!!.edit()
+    editor.remove(key)
+    editor.apply()
+  }
+
+  fun deleteAll() {
+    ensureInitialized()
+
+    val editor = preferences!!.edit()
+    editor.clear()
+    if (!getUseEncryptedSharedPreferences()) {
+      storageCipherFactory!!.storeCurrentAlgorithms(editor)
+    }
+    editor.apply()
+  }
+
+  fun setOptions(options: Map<String, String>) {
+    this.options = options.toMutableMap()
+  }
+
+  fun ensureOptions() {
+    val name = options["sharedPreferencesName"]
+    if (!name.isNullOrEmpty()) {
+      sharedPreferencesName = name
+    }
+
+    val prefix = options["preferencesKeyPrefix"]
+    if (!prefix.isNullOrEmpty()) {
+      elementPreferencesKeyPrefix = prefix
+    }
+  }
+
+  private fun ensureInitialized() {
+    // Check if already initialized.
+    // TODO: Disable for now because this will break mixed usage of secureSharedPreference
+//        if (preferences != null) return;
+
+    ensureOptions()
+
+    val nonEncryptedPreferences = applicationContext.getSharedPreferences(
+      sharedPreferencesName,
+      Context.MODE_PRIVATE,
+    )
+    if (storageCipher == null) {
+      try {
+        initStorageCipher(nonEncryptedPreferences)
+      } catch (e: Exception) {
+        Log.e(TAG, "StorageCipher initialization failed", e)
+      }
+    }
+    if (getUseEncryptedSharedPreferences()) {
+      try {
+        preferences = initializeEncryptedSharedPreferencesManager(applicationContext)
+        checkAndMigrateToEncrypted(nonEncryptedPreferences, preferences!!)
+      } catch (e: Exception) {
+        Log.e(TAG, "EncryptedSharedPreferences initialization failed", e)
+        preferences = nonEncryptedPreferences
+        failedToUseEncryptedSharedPreferences = true
+      }
+    } else {
+      preferences = nonEncryptedPreferences
+    }
+  }
+
+  private fun initStorageCipher(source: SharedPreferences) {
+    storageCipherFactory = StorageCipherFactory(source, options.toMap())
+    if (getUseEncryptedSharedPreferences()) {
+      storageCipher = storageCipherFactory!!.getSavedStorageCipher(applicationContext)
+    } else if (storageCipherFactory!!.requiresReEncryption()) {
+      reEncryptPreferences(storageCipherFactory!!, source)
+    } else {
+      storageCipher = storageCipherFactory!!.getCurrentStorageCipher(applicationContext)
+    }
+  }
+
+  private fun reEncryptPreferences(
+    storageCipherFactory: StorageCipherFactory,
+    source: SharedPreferences,
+  ) {
+    try {
+      storageCipher = storageCipherFactory.getSavedStorageCipher(applicationContext)
+      val cache = mutableMapOf<String, String?>()
+      for (entry in source.all.entries) {
+        val v: Any? = entry.value
+        val key = entry.key
+        if (v is String && key.contains(elementPreferencesKeyPrefix)) {
+          val decodedValue = decodeRawValue(v)
+          cache.put(key, decodedValue)
+        }
+      }
+      storageCipher = storageCipherFactory.getCurrentStorageCipher(applicationContext)
+      val editor = source.edit()
+      for (entry in cache.entries) {
+        val result = storageCipher!!.encrypt(entry.value!!.toByteArray(charset))
+        editor.putString(entry.key, Base64.encodeToString(result, 0))
+      }
+      storageCipherFactory.storeCurrentAlgorithms(editor)
+      editor.apply()
+    } catch (e: Exception) {
+      Log.e(TAG, "re-encryption failed", e)
+      storageCipher = storageCipherFactory.getSavedStorageCipher(applicationContext)
+    }
+  }
+
+  private fun checkAndMigrateToEncrypted(source: SharedPreferences, target: SharedPreferences) {
+    try {
+      for (entry in source.all.entries) {
+        val v: Any? = entry.value
+        val key = entry.key
+        if (v is String && key.contains(elementPreferencesKeyPrefix)) {
+          val decodedValue = decodeRawValue(v)
+          target.edit().putString(key, (decodedValue)).apply()
+          source.edit().remove(key).apply()
+        }
+      }
+      val sourceEditor = source.edit()
+      storageCipherFactory!!.removeCurrentAlgorithms(sourceEditor)
+      sourceEditor.apply()
+    } catch (e: Exception) {
+      Log.e(TAG, "Data migration failed", e)
+    }
+  }
+
+  private fun initializeEncryptedSharedPreferencesManager(context: Context): SharedPreferences {
+    val key = MasterKey.Builder(context)
+      .setKeyGenParameterSpec(
+        KeyGenParameterSpec.Builder(
+          MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+          KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        )
+          .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+          .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+          .setKeySize(256).build()
+      )
+      .build()
+    return EncryptedSharedPreferences.create(
+      context,
+      sharedPreferencesName,
+      key,
+      EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+      EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+  }
+
+  private fun decodeRawValue(value: String): String {
+    val data = Base64.decode(value, 0)
+    val result = storageCipher!!.decrypt(data)
+
+    return String(result, charset)
+  }
+
+  companion object {
+    private const val TAG = "SecureStorageAndroid"
+
+    private val charset: Charset = StandardCharsets.UTF_8
+  }
+}
---
+++ flutter_secure_storage_x/android/src/main/java/com/it_nomads/fluttersecurestorage/FlutterSecureStoragePlugin.kt
@@ -0,0 +1,178 @@
+package com.it_nomads.fluttersecurestorage
+
+import android.content.Context
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.util.Log
+import com.it_nomads.fluttersecurestorage.FlutterSecureStoragePlugin.MethodResultWrapper
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import java.io.FileNotFoundException
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.lang.Exception
+
+class FlutterSecureStoragePlugin : MethodCallHandler, FlutterPlugin {
+  private var channel: MethodChannel? = null
+  private var secureStorage: FlutterSecureStorage? = null
+  private var workerThread: HandlerThread? = null
+  private var workerThreadHandler: Handler? = null
+
+  fun initInstance(messenger: BinaryMessenger, context: Context) {
+    try {
+      secureStorage = FlutterSecureStorage(context)
+
+      workerThread = HandlerThread("com.it_nomads.fluttersecurestorage.worker")
+      workerThread!!.start()
+      workerThreadHandler = Handler(workerThread!!.getLooper())
+
+      channel = MethodChannel(messenger, "plugins.it_nomads.com/flutter_secure_storage")
+      channel!!.setMethodCallHandler(this)
+    } catch (e: Exception) {
+      Log.e(TAG, "Registration failed", e)
+    }
+  }
+
+  override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+    initInstance(binding.binaryMessenger, binding.applicationContext)
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+    if (channel != null) {
+      workerThread!!.quitSafely()
+      workerThread = null
+
+      channel!!.setMethodCallHandler(null)
+      channel = null
+    }
+    secureStorage = null
+  }
+
+  override fun onMethodCall(call: MethodCall, rawResult: MethodChannel.Result) {
+    val result = MethodResultWrapper(rawResult)
+    // Run all method calls inside the worker thread instead of the platform thread.
+    workerThreadHandler!!.post(MethodRunner(call, result))
+  }
+
+  private fun getKeyFromCall(call: MethodCall): String {
+    val arguments = call.arguments as Map<String, *>
+    return secureStorage!!.addPrefixToKey(arguments["key"] as String)
+  }
+
+  private fun getValueFromCall(call: MethodCall): String {
+    val arguments = call.arguments as Map<String, *>
+    return arguments["value"] as String
+  }
+
+  /**
+   * MethodChannel.Result wrapper that responds on the platform thread.
+   */
+  private class MethodResultWrapper(
+    private val methodResult: MethodChannel.Result,
+  ) : MethodChannel.Result {
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun success(result: Any?) {
+      handler.post(Runnable { methodResult.success(result) })
+    }
+
+    override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
+      handler.post(Runnable { methodResult.error(errorCode, errorMessage, errorDetails) })
+    }
+
+    override fun notImplemented() {
+      handler.post(Runnable { methodResult.notImplemented() })
+    }
+  }
+
+  /**
+   * Wraps the functionality of onMethodCall() in a Runnable for execution in the worker thread.
+   */
+  private inner class MethodRunner(
+    private val call: MethodCall,
+    private val result: MethodChannel.Result,
+  ) : Runnable {
+
+    override fun run() {
+      var resetOnError = false
+      try {
+        secureStorage!!.setOptions((call.arguments as Map<String, *>)["options"] as Map<String, String>)
+        secureStorage!!.ensureOptions()
+        resetOnError = secureStorage!!.getResetOnError()
+        when (call.method) {
+          "write" -> {
+            val key = getKeyFromCall(call)
+            val value = getValueFromCall(call)
+
+            secureStorage!!.write(key, value)
+            result.success(null)
+          }
+
+          "read" -> {
+            val key = getKeyFromCall(call)
+
+            if (secureStorage!!.containsKey(key)) {
+              val value = secureStorage!!.read(key)
+              result.success(value)
+            } else {
+              result.success(null)
+            }
+          }
+
+          "readAll" -> {
+            result.success(secureStorage!!.readAll())
+          }
+
+          "containsKey" -> {
+            val key = getKeyFromCall(call)
+
+            val containsKey = secureStorage!!.containsKey(key)
+            result.success(containsKey)
+          }
+
+          "delete" -> {
+            val key = getKeyFromCall(call)
+
+            secureStorage!!.delete(key)
+            result.success(null)
+          }
+
+          "deleteAll" -> {
+            secureStorage!!.deleteAll()
+            result.success(null)
+          }
+
+          else -> result.notImplemented()
+        }
+      } catch (e: FileNotFoundException) {
+        Log.i("Creating sharedPrefs", e.message!!)
+      } catch (e: Exception) {
+        if (resetOnError) {
+          try {
+            secureStorage!!.deleteAll()
+            result.success("Data has been reset")
+          } catch (ex: Exception) {
+            handleException(ex)
+          }
+        } else {
+          handleException(e)
+        }
+      }
+    }
+
+    private fun handleException(e: Exception) {
+      val stringWriter = StringWriter()
+      e.printStackTrace(PrintWriter(stringWriter))
+      result.error("Exception encountered", call.method, stringWriter.toString())
+    }
+  }
+
+  companion object {
+    private const val TAG = "FlutterSecureStoragePl"
+  }
+}
