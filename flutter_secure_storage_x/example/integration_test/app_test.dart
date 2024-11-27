795bb24cc7a055c754da468041fbc6827b69ca04
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-11-27 13:02:33 +0900

    chore: testTextInput.register()

--- flutter_secure_storage_x/example/integration_test/app_test.dart
+++ flutter_secure_storage_x/example/integration_test/app_test.dart
@@ -6,7 +6,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  binding.testTextInput.register();
 
   testWidgets('Secure Storage Example', (tester) async {
     await tester.pumpWidget(
